868fac59a02046626105dc43f96850a1d02d3e6e
gwb <gw.basse@gmail.com>
2024-01-21 10:09:44 +0000

    Improve syntax highlighting for k-mode.

--- gwb-lisp/gwb-kmode.el
+++ gwb-lisp/gwb-kmode.el
@@ -1,4 +1,6 @@
 
+(defvar k-mode--verbs (string-to-list "+-*<=>|#&^@._'$?!%:\\"))
+
 (defvar k-mode--syntax-table
   (let ((table (make-syntax-table)))
     ;; (modify-syntax-entry ?+ "." table)
@@ -9,16 +11,21 @@
     ;; (modify-syntax-entry ?: "." table)
     ;; (modify-syntax-entry ?/ ". 2")
     ;; (modify-syntax-entry ?\s "- 1")
+    (dolist (s k-mode--verbs)
+      (modify-syntax-entry s "." table))
     (modify-syntax-entry ?\/ "<" table)
     (modify-syntax-entry ?\n ">" table)
     table)
   "Syntax table for `k-mode'.")
 
-
-
-  (defvar k-mode--font-lock-defaults
-    `((("[+/]" . 'font-lock-keyword-face)))  
-    )
+(defvar k-mode--font-lock-defaults
+  `((("[;]" . 'font-lock-warning-face)
+     ("[()]" . 'font-lock-bracket-face)
+     ("\\([a-zA-Z]+[a-zA-Z0-9]*\\) *:" . (1 font-lock-variable-name-face)) ; var assignment
+     ("[^a-zA-Z0-9]\\(x\\|y\\|z\\)[^a-zA-Z0-9]" . (1 font-lock-keyword-face)) ; x y z in {}
+     ("[a-zA-Z]+[a-zA-Z0-9]*" . 'font-lock-variable-use-face) ; var assignment
+     )
+    nil nil nil))
 
 (defvar k-mode--syntax-propertize
   (syntax-propertize-rules
